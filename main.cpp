763bf1bbd7bca9af7b782a67532bd8813ea893c0
Ho Cheung <hocheung20@gmail.com>
2013-06-03 18:59:10 -0500

    ENH: Add itkProbe

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include "itkVector.h"
 #include "itkImage.h"
+#include "itkTimeProbe.h"
 
 int main()
 {
+  itk::TimeProbe clock;
   typedef itk::Vector< unsigned char, 1 > VectorType;
   typedef itk::Image< VectorType, 3 > ImageType;
   ImageType::Pointer image = ImageType::New();
@@ -18,7 +20,11 @@ int main()
   ImageType::RegionType region(start, size);
   
   image->SetRegions(region);
+  clock.Start();
   image->Allocate();
+  clock.Stop();
+  
+  std::cout << "Allocation took " << clock.GetTotal() << " seconds" << std::endl;
   
   return 0;
 }
