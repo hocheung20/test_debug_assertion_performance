de50ebf15f43551ff8bc5d15ef3ff728b6536b2d
Ho Cheung <hocheung20@gmail.com>
2013-06-03 23:50:06 -0500

    BUG: Actually use the vnl_vector fill

--- main.cpp
+++ main.cpp
@@ -1,30 +1,19 @@
-#include "itkVector.h"
-#include "itkImage.h"
 #include "itkTimeProbe.h"
+#include "itkArray.h"
 
 int main()
 {
-  itk::TimeProbe clock;
-  typedef itk::Vector< unsigned char, 1 > VectorType;
-  typedef itk::Image< VectorType, 3 > ImageType;
-  ImageType::Pointer image = ImageType::New();
-  
-  ImageType::IndexType start;
-  start.Fill(0);
-  
-  ImageType::SizeType size;
-  size[0] = 10 * 1024;
-  size[1] = 1024;
-  size[2] = 1024;
-  
-  ImageType::RegionType region(start, size);
-  
-  image->SetRegions(region);
-  clock.Start();
-  image->Allocate();
-  clock.Stop();
-  
-  std::cout << "Allocation took " << clock.GetTotal() << " seconds" << std::endl;
-  
-  return 0;
+	typedef itk::Array< unsigned char > ArrayType;
+	itk::TimeProbe clock;  
+
+	ArrayType array(4*1024*1024*1024); //Create 1 GB array
+	
+	clock.Start();
+	for (unsigned int k = 0; k < 1000000000; ++k)
+		array.fill(0);
+	clock.Stop();
+
+	std::cout << "Allocation took " << clock.GetTotal() << " seconds" << std::endl;
+
+	return 0;
 }
