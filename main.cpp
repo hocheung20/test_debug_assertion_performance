21b91195d5b81dd6be53e62bfc9ca9ab85f8cd59
Ho Cheung <hocheung20@gmail.com>
2013-06-04 01:38:52 -0500

    BUG: Overflow error in array size

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ int main()
 	typedef itk::Array< unsigned char > ArrayType;
 	itk::TimeProbe clock;  
 
-	ArrayType array(4*1024*1024*1024); //Create 1 GB array
+	ArrayType array(6); //Create array of 6 elements
 	
 	clock.Start();
-	for (unsigned int k = 0; k < 1000000000; ++k)
+	for (unsigned int k = 0; k < 1024*1024*1024; ++k)
 		array.fill(0);
 	clock.Stop();
 
